4570de0fb55b9cdebf038f1646274b66a2730ba0
JasonXB <jasonxportfolio@gmail.com>
2022-12-28 13:03:24 -0500

    Custom Validators

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const createUser = require("./mongo/setup");
 
+mongoose.set("strictQuery", false);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.post("/", (req, res) => {
-  const { name, age, password } = req.body;
-  console.log(req.body);
-  // Add some logic that only allows things to happen if conditions are met
-  if (password !== "strong enough") {
-    return res.status(400).end();
-  }
+  const { name, age } = req.body;
   // If password is "strong enough", create a new user
-  createUser(name, age, password);
+  createUser(name, age);
   return res.status(200).send({ success: true });
 });
 
-// Listen for events on these endpoints (i think this method is for development-only)
 app.listen(3000);
