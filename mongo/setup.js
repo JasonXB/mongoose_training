50669cd8316278a46bba672c3344f7075b86e407
JasonXB <jasonxportfolio@gmail.com>
2022-12-29 19:58:24 -0500

    Building the students example

--- mongo/setup.js
+++ mongo/setup.js
@@ -4,10 +4,29 @@ mongoose.connect(
 ); // connect to Atlas cluster
 
 // Create schema for user document objects
-const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true, // mandatory age property. Must be a number if included
+  },
+  age: {
+    type: Number,
+    required: true, // mandatory age property. Must be a number if included
+  },
+  grades: {
+    type: [Number], // array of numbers
+    required: true,
+  },
+  faculty: {
+    type: String,
+    enum: ["FEAS", "FBIT", "FOE", "FOS", "FSSH"], // limited to these strings
+    required: true,
+  },
+  average: {
+    type: Number,
+    required: true,
+  },
 });
-// Create a User model to perform CRUD operations on the "Users" collection
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+// Create a User model to perform CRUD operations on the "Students" collection
+const Student = mongoose.model("User", studentSchema);
+module.exports = Student;
--- script.js
+++ script.js
@@ -7,13 +7,18 @@ mongoose.set("strictQuery", false);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// CREATE a new document
-// Make a post request with  { name: String, age: Number} payload and save it to DB
+// CREATE a new student document
+// Make a post request with  { name, age, grades, faculty } payload
 app.post("/", async (req, res) => {
-  const { name, age } = req.body;
+  // Extract data from request body
+  const { name, age, grades, faculty } = req.body;
+  console.log(req.body);
+  // Calc average from grades array
+  const sum = grades.reduce((a, b) => a + b, 0);
+  const average = sum / grades.length || 0;
+  // Create a new user with the info you've got
   try {
-    await User.create({ name, age }); // create new user
-    if (!name || !age) throw new Error(); // if name/age is not provided, use catch block
+    await User.create({ name, age, grades, faculty, average }); // create new user
     return res.status(200).send({ success: true });
   } catch (error) {
     return res.status(401).send({ failure: true });
@@ -25,49 +30,10 @@ app.post("/", async (req, res) => {
 app.get("/", async (req, res) => {
   const { name } = req.query;
   try {
-    const matchesList = await User.where("age")
-      .gt(1)
-      .limit(3)
-      .select("name")
-      .select("_id");
-    if (matchesList.length === 0) throw new Error(); // if no matches are found, throw an error
+    const matchesList = [];
     return res.status(200).send({ success: true, matchesList });
   } catch (error) {
     return res.status(401).send({ failure: true });
-  }
-});
-
-// UPDATE an existing document's age
-// Make a put request with { name: String, age: Number} payload and save it to DB
-app.put("/", async (req, res) => {
-  const { name, age } = req.body;
-  try {
-    const document = await User.findOne({ name }); // find document (null if not found)
-    if (document === null) throw new Error(); // use catch block if we get no matches
-    // Edit the document then save changes to the DB
-    document.age = age; // change age to new value from body
-    await document.save();
-    return res.status(200).send({ success: true, document });
-  } catch (error) {
-    return res.status(401).send({ failure: true });
-  }
-});
-
-// DELETE an existing document
-// Make a put request with { name: String } payload and wipe any matches from the DB
-app.delete("/", async (req, res) => {
-  const { name } = req.body;
-  try {
-    const result = await User.deleteOne({ name }); // find document (null if not found)
-    console.log(result); // { acknowledged: Boolean, deletedCount: 0 if no matches found
-    if (result.deletedCount === 0) throw { noMatch: true };
-    return res.status(200).send({ success: true });
-  } catch (error) {
-    if (error.noMatch)
-      return res
-        .status(401)
-        .send({ message: "No account found with that name" });
-    return res.status(401).send(error);
   }
 });
 
