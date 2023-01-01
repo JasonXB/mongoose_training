6fb6a9a55658480a11bdac44087372b67f07a8d0
JasonXB <jasonxportfolio@gmail.com>
2023-01-01 13:47:52 -0500

    Middleware demo success

--- mongo/setup.js
+++ mongo/setup.js
@@ -5,9 +5,22 @@ mongoose.connect(
 
 // Create schema for user document objects
 const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  updatedAt: { type: Date, required: true },
 });
+
+// Middleware function runs before each .save()
+userSchema.pre("save", function (next) {
+  this.updatedAt = Date.now();
+  next(); // pass control to next function
+});
+// Middleware function runs after each .save()
+userSchema.post("save", function (doc, next) {
+  console.log("UPDATED USER DATA");
+  next(); // pass control to next function
+});
+
 // Create a User model to perform CRUD operations on the "Users" collection
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- script.js
+++ script.js
@@ -11,8 +11,9 @@ app.use(express.urlencoded({ extended: false }));
 // Make a post request with  { name: String, age: Number} payload and save it to DB
 app.post("/", async (req, res) => {
   const { name, age } = req.body;
+  const updatedAt = new Date();
   try {
-    await User.create({ name, age }); // create new user
+    await User.create({ name, age, updatedAt }); // create new user
     if (!name || !age) throw new Error(); // if name/age is not provided, use catch block
     return res.status(200).send({ success: true });
   } catch (error) {
@@ -20,55 +21,21 @@ app.post("/", async (req, res) => {
   }
 });
 
-// READ an existing document then return it
-// visit http://localhost:3000/?name=amy to search for document with name: "amy"
-app.get("/", async (req, res) => {
-  const { name } = req.query;
-  try {
-    const matchesList = await User.where("age")
-      .gt(1)
-      .limit(3)
-      .select("name")
-      .select("_id");
-    if (matchesList.length === 0) throw new Error(); // if no matches are found, throw an error
-    return res.status(200).send({ success: true, matchesList });
-  } catch (error) {
-    return res.status(401).send({ failure: true });
-  }
-});
-
-// UPDATE an existing document's age
-// Make a put request with { name: String, age: Number} payload and save it to DB
+// UPDATE an existing user's age
 app.put("/", async (req, res) => {
-  const { name, age } = req.body;
   try {
-    const document = await User.findOne({ name }); // find document (null if not found)
-    if (document === null) throw new Error(); // use catch block if we get no matches
-    // Edit the document then save changes to the DB
-    document.age = age; // change age to new value from body
-    await document.save();
-    return res.status(200).send({ success: true, document });
+    // Find a user whose name matches the name property from the request body
+    const { name, age } = req.body;
+    const user = await User.findOne({ name });
+    if (user === null) throw new Error(); // use catch block if we get no matches
+    // Change age value on document then save
+    user.age = age;
+    await user.save();
+    // Middleware changes updatedAt value silently
+    return res.status(200).send({ success: true, user });
   } catch (error) {
     return res.status(401).send({ failure: true });
   }
 });
 
-// DELETE an existing document
-// Make a put request with { name: String } payload and wipe any matches from the DB
-app.delete("/", async (req, res) => {
-  const { name } = req.body;
-  try {
-    const result = await User.deleteOne({ name }); // find document (null if not found)
-    console.log(result); // { acknowledged: Boolean, deletedCount: 0 if no matches found
-    if (result.deletedCount === 0) throw { noMatch: true };
-    return res.status(200).send({ success: true });
-  } catch (error) {
-    if (error.noMatch)
-      return res
-        .status(401)
-        .send({ message: "No account found with that name" });
-    return res.status(401).send(error);
-  }
-});
-
-app.listen(3000);
+app.listen(5500);
