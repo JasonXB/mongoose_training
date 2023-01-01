2887855e42610a8ff4ff743ea1e464a9004ed75f
JasonXB <jasonxportfolio@gmail.com>
2023-01-01 10:58:59 -0500

    Instance and statics method demo

--- mongo/setup.js
+++ mongo/setup.js
@@ -27,6 +27,27 @@ const studentSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+// SET UP INSTANCE METHOD: Can be called on individual documents
+studentSchema.methods.letterAverage = function () {
+  const numAvg = this.average;
+  if (numAvg < 50) return "F";
+  else if (numAvg < 60) return "D";
+  else if (numAvg < 70) return "C";
+  else if (numAvg < 80) return "B";
+  else if (numAvg < 90) return "A";
+  else if (numAvg < 100) return "S";
+  else return null;
+};
+
+// SET UP STATIC METHOD: Premade query to be used on a model
+studentSchema.statics.findStudentByName = async function (name) {
+  return this.findOne({ name });
+};
+studentSchema.statics.findFacultyMembers = async function (faculty) {
+  return this.where("faculty").equals(faculty);
+};
+
 // Create a User model to perform CRUD operations on the "Students" collection
-const Student = mongoose.model("User", studentSchema);
+const Student = mongoose.model("Student", studentSchema);
 module.exports = Student;
--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const User = require("./mongo/setup");
+const cors = require("cors");
+const Student = require("./mongo/setup");
 
-mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "http://localhost:3000", // client side application port
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  })
+);
+
+// visit http://localhost:3000/?name=amy to search for document with name: "amy"
+app.get("/", async (req, res) => {
+  // console.log(req.query.name)
+  try {
+    // GET STUDENT DOCUMENT USING THE STATIC METHOD (which acts as a premade query)
+    const studentDoc = await Student.findStudentByName(req.query.name); // null if not found
+    console.log(studentDoc);
+    // CALC & RETURN STUDENT'S LETTER GRADE USING THE INSTANCE METHOD
+    return res.status(200).send({ letterAverage: studentDoc.letterAverage() }); // prettier-ignore
+  } catch (error) {
+    return res.status(401).send(error);
+  }
+});
 
 // CREATE a new student document
 // Make a post request with  { name, age, grades, faculty } payload
 app.post("/", async (req, res) => {
   // Extract data from request body
   const { name, age, grades, faculty } = req.body;
-  console.log(req.body);
   // Calc average from grades array
   const sum = grades.reduce((a, b) => a + b, 0);
-  const average = sum / grades.length || 0;
-  // Create a new user with the info you've got
+  let average = sum / grades.length || 0;
+  average = average.toFixed(1);
+  // Create a new Student with the info you've got
   try {
-    await User.create({ name, age, grades, faculty, average }); // create new user
+    await Student.create({ name, age, grades, faculty, average }); // create new Student
     return res.status(200).send({ success: true });
   } catch (error) {
     return res.status(401).send({ failure: true });
   }
 });
 
-// READ an existing document then return it
-// visit http://localhost:3000/?name=amy to search for document with name: "amy"
-app.get("/", async (req, res) => {
-  const { name } = req.query;
-  try {
-    const matchesList = [];
-    return res.status(200).send({ success: true, matchesList });
-  } catch (error) {
-    return res.status(401).send({ failure: true });
-  }
-});
-
-app.listen(3000);
+app.listen(5500);
